a6e249177c8f53a246b4f60a25cc8ed0d3e743ae
anirva-m-r <anirva@buyexchange.in>
2025-11-12 20:15:21 +0530

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <main className="flex flex-col items-center justify-center min-h-[80vh] max-w-5xl mx-auto px-6 text-center">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <nav className="w-full bg-white/70 backdrop-blur-md shadow-sm border-b border-gray-200">
+      <div className="max-w-6xl mx-auto px-6 py-3 flex justify-between items-center">
+        <h1 className="font-bold text-lg text-blue-600">Next JS</h1>
+        <div className="space-x-4">
+          <a href="#" className="text-gray-700 hover:text-blue-600">Footer</a>
+          
+        </div>
+      </div>
+    </nav>
+  );
+}
