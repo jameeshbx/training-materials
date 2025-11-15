d74b7983435b473998cca66246182f535919aa8d
eldhoseER <eldhose@buyexchange.in>
2025-11-15 14:37:55 +0530

    Updated prisma relations and timeEntry service

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,44 +1,5 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
+import { userController } from "@/controller/userController";
 
-
-
-{/*Create user */}
-
-
-
-const prisma = new PrismaClient();
-
-export const POST = async (req: Request) => {
-  try {
-    const body = await req.json();
-    const { name, email } = body;
-
-    const addUser = await prisma.user.create({
-      data: {
-        name: name,
-        email: email,
-      },
-    });
-
-    return NextResponse.json(addUser, { status: 201 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
-  }
-};
-
-
-{/*Get all Users */}
-
-export const GET=async()=>{
-  try {
-    const findAlluser=await prisma.user.findMany()
-    console.log(findAlluser)
-    return NextResponse.json({succes:true,findAlluser})
-  } catch (error) {
-        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
-
-  }
-}
+export const GET = userController.findAll;
+export const POST = userController.create;
